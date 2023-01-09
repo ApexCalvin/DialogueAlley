83ac6f9d465f604da9be6f0775d13aebcba2a569
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2023-01-09 11:04:17 -0500

    added ENTITY and SERVICE annotations on CrossReference MVC

--- backend_v2/DialogueAlley/src/main/java/com/formosa/DialogueAlley/services/PostHashtagCrossReferenceServices.java
+++ backend_v2/DialogueAlley/src/main/java/com/formosa/DialogueAlley/services/PostHashtagCrossReferenceServices.java
@@ -3,9 +3,11 @@ package com.formosa.DialogueAlley.services;
 import com.formosa.DialogueAlley.model.PostHashtagCrossReference;
 import com.formosa.DialogueAlley.repository.PostHashtagCrossReferenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PostHashtagCrossReferenceServices {
     @Autowired
     PostHashtagCrossReferenceRepository crossReference;
