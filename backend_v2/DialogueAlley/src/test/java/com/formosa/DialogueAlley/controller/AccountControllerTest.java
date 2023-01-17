19f13b26e5a9f16ca4cd3a9a2759c702cfa8e39c
John <john@Zipcoders-MacBook-Pro-46.local>
2023-01-17 13:20:30 -0500

    Account Controller Tests

--- backend_v2/DialogueAlley/src/test/java/com/formosa/DialogueAlley/controller/AccountControllerTest.java
+++ backend_v2/DialogueAlley/src/test/java/com/formosa/DialogueAlley/controller/AccountControllerTest.java
@@ -10,9 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -20,16 +21,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(AccountController.class)
+@AutoConfigureMockMvc
 class AccountControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     PostRepository postRepository;
 
     @MockBean
@@ -43,16 +48,17 @@ class AccountControllerTest {
 
     AccountControllerTest() throws JsonProcessingException {
     }
+    Account account= new Account();
+    ObjectMapper objectMapper = new ObjectMapper();
+    String accountJson = objectMapper.writeValueAsString(account);
 
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(accountController).build();
+        MockitoAnnotations.initMocks(this);
+        account.setAccount_id(123);
     }
 
-
-    Account account= new Account();
-    ObjectMapper objectMapper = new ObjectMapper();
-    String json = objectMapper.writeValueAsString(account);
 
     @Test
     void shouldCreateMockMvc() {
@@ -61,28 +67,56 @@ class AccountControllerTest {
 
     @Test
     void addAccount() throws Exception {
-            mockMvc.perform(post("/add")
+            mockMvc.perform(post("/account/add")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
+                .content(accountJson))
                 .andExpect(status().isOk())
                 .andReturn();
-               // .andExpect(content())
     }
 
     @Test
-    void getAllAccounts() {
+    void getAllAccounts() throws Exception{
+        mockMvc.perform(get("/account/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+
     }
 
     @Test
-    void getAccountById() {
+    void getAccountById() throws Exception {
+        when(accountServices.getAccountById(anyInt())).thenReturn(account);
+
+        mockMvc.perform(get("/account/123")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.account_id").value(123));
+    }
+    @Test
+    void getAccountByIdFailed() throws Exception {
+        //when(accountServices.getAccountById(anyInt())).thenReturn(account);
+
+        mockMvc.perform(get("/account/xx")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().is4xxClientError());
+    }
+    @Test
+    void updateAccount() throws Exception {
+        mockMvc.perform(put("/account/123")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(accountJson))
+                        .andExpect(status().isOk())
+                        .andReturn();
     }
 
     @Test
-    void updateAccount() {
-    }
-
-    @Test
-    void deleteAccountById() {
+    void deleteAccountById() throws Exception {
+        mockMvc.perform(delete("/account/123")
+                        .param("account_id","123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andReturn();
     }
 
     @Test
--- backend_v2/DialogueAlley/src/test/java/com/formosa/DialogueAlley/controller/CommentControllerTest.java
+++ backend_v2/DialogueAlley/src/test/java/com/formosa/DialogueAlley/controller/CommentControllerTest.java
@@ -1,33 +1,110 @@
 package com.formosa.DialogueAlley.controller;
 
+import com.formosa.DialogueAlley.model.Comment;
+import com.formosa.DialogueAlley.model.DTO.CommentSaveDTO;
+import com.formosa.DialogueAlley.model.DTO.PostListDTO;
+import com.formosa.DialogueAlley.repository.CommentRepository;
+import com.formosa.DialogueAlley.services.CommentServices;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import static java.lang.reflect.Array.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(CommentController.class)
 class CommentControllerTest {
+
+    MockMvc mockMvc;
+
+    @Mock
+    CommentServices commentServices;
+
+    @Mock
+    CommentRepository commentRepository;
+
+    @InjectMocks
+    CommentController commentController;
+
+
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(commentController).build();
     }
 
     @Test
     void addComment() {
+        CommentSaveDTO comment = new CommentSaveDTO();
+        when(commentServices.saveComment(comment)).thenReturn(true);
+        String result = commentController.addComment(comment);
+        assertEquals("Comment has been created.", result);
+
+        when(commentServices.saveComment(comment)).thenReturn(false);
+        result = commentController.addComment(comment);
+        assertEquals("Failed to create a comment.", result);
     }
 
     @Test
     void getAllComments() {
+        Comment comment = new Comment();
+        when(commentServices.getCommentById(1)).thenReturn(comment);
+        ResponseEntity<Comment> result = commentController.getCommentById(1);
+        assertEquals(comment, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+
+//        when(commentServices.getCommentById(1)).thenThrow(NoSuchElementException.class);
+//        result = commentController.getCommentById(1);
+//        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
     }
 
     @Test
     void getCommentById() {
+        Comment expectedComment = new Comment();
+        expectedComment.setComment_id(1);
+        when(commentServices.getCommentById(1)).thenReturn(expectedComment);
+
+        ResponseEntity<Comment> result = commentController.getCommentById(1);
+
+        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+        Assertions.assertEquals(expectedComment, result.getBody());
+        verify(commentServices, times(1)).getCommentById(1);
     }
 
     @Test
-    void deleteCCommentById() {
+    void deleteCommentById() {
+        commentController.deleteCCommentById(1);
+        verify(commentServices, times(1)).deleteCommentById(1);
     }
 
     @Test
     void findCommentsByPost() {
+        List<PostListDTO> comments = new ArrayList<>();
+        comments.add(new PostListDTO());
+        when(commentRepository.findAllCommentsByPostId(1)).thenReturn(comments);
+        List<PostListDTO> result = commentController.findCommentsByPost(1);
+        assertEquals(comments, result);
     }
 }
